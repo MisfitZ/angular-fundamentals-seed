77670eb3121d749df4b594767023dd90e99463f4
Jeremiah Zuraff <jeremiah.zuraff@gmail.com>
2018-03-02 16:32:40 -0600

    initial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <div class="app">
-      Hello!
-    </div>
+      <div class="app">
+        <button (click)="handleClick()"> What is Ellie </button>
+        <input type="text" >
+        <div>
+          <div>{{ name }}
+        </div>
+      </div>
   `
 })
 export class AppComponent {
+  name: string = '';
 
+  handleClick(){
+    this.name += 'Ellianna is a turd'
+    
+  }
+  
 }
